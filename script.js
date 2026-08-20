9de06a68f7ca9a9f24c2f35f7cfc2e5be34ce99e
Humayun Parwez <humayunparwez@gmail.com>
2026-08-21 01:06:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+// ===============================
+// FoodBridge - Supabase Connection
+// ===============================
+
+const SUPABASE_URL = "https://fnnogzkxdhqosafnwchl.supabase.co";
+
+// Paste your PUBLISHABLE key between the quotes
+const SUPABASE_KEY =" sb_publishable_GnbcANoxYMbFwcqj2B--ZQ_pnkHJERx";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+
+// ===============================
+// Get Started Button
+// ===============================
+
 function getStarted() {
-
-    alert(
-        "Welcome to FoodBridge! 🍱"
-    );
-
+    alert("Welcome to FoodBridge! 🍔");
 }
